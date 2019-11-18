189a4faedf2165f0438bf3701d97a8c96b08869e
dpetrovs <dpetrovs@evolutiongaming.com>
2019-11-18 21:25:35 +0200

    Refactoring of board initial state creator

--- src/Chess.cpp
+++ src/Chess.cpp
@@ -48,35 +48,34 @@ void Board::setDefaultBoardState()
     // Populate with pawns for white and black.
     for (int i = 'A'; i < 'I'; ++i)
     {
-        boardState.push_back(Piece(Piece::Type::PAWN, Piece::Color::WHITE, (char)i, 2));
-        boardState.push_back(Piece(Piece::Type::PAWN, Piece::Color::BLACK, (char)i, 7));
+        addPiece(Piece(Piece::Type::PAWN, Piece::Color::WHITE, (char)i, 2));
+        addPiece(Piece(Piece::Type::PAWN, Piece::Color::BLACK, (char)i, 7));
     }
     /* White pieces */
-    boardState.push_back(Piece(Piece::Type::ROOK, Piece::Color::WHITE, 'A', 1));
-    boardState.push_back(Piece(Piece::Type::ROOK, Piece::Color::WHITE, 'H', 1));
+    addPiece(Piece(Piece::Type::ROOK, Piece::Color::WHITE, 'A', 1));
+    addPiece(Piece(Piece::Type::ROOK, Piece::Color::WHITE, 'H', 1));
 
-    boardState.push_back(Piece(Piece::Type::KNIGHT, Piece::Color::WHITE, 'B', 1));
-    boardState.push_back(Piece(Piece::Type::KNIGHT, Piece::Color::WHITE, 'G', 1));
+    addPiece(Piece(Piece::Type::KNIGHT, Piece::Color::WHITE, 'B', 1));
+    addPiece(Piece(Piece::Type::KNIGHT, Piece::Color::WHITE, 'G', 1));
 
-    boardState.push_back(Piece(Piece::Type::BISHOP, Piece::Color::WHITE, 'C', 1));
-    boardState.push_back(Piece(Piece::Type::BISHOP, Piece::Color::WHITE, 'F', 1));
+    addPiece(Piece(Piece::Type::BISHOP, Piece::Color::WHITE, 'C', 1));
+    addPiece(Piece(Piece::Type::BISHOP, Piece::Color::WHITE, 'F', 1));
 
-    boardState.push_back(Piece(Piece::Type::QUEEN, Piece::Color::WHITE, 'D', 1));
-    boardState.push_back(Piece(Piece::Type::KING, Piece::Color::WHITE, 'E', 1));
+    addPiece(Piece(Piece::Type::QUEEN, Piece::Color::WHITE, 'D', 1));
+    addPiece(Piece(Piece::Type::KING, Piece::Color::WHITE, 'E', 1));
 
     /* Black pieces */
-    boardState.push_back(Piece(Piece::Type::ROOK, Piece::Color::BLACK, 'A', 1));
-    boardState.push_back(Piece(Piece::Type::ROOK, Piece::Color::BLACK, 'H', 1));
+    addPiece(Piece(Piece::Type::ROOK, Piece::Color::BLACK, 'A', 1));
+    addPiece(Piece(Piece::Type::ROOK, Piece::Color::BLACK, 'H', 1));
 
-    boardState.push_back(Piece(Piece::Type::KNIGHT, Piece::Color::BLACK, 'B', 1));
-    boardState.push_back(Piece(Piece::Type::KNIGHT, Piece::Color::BLACK, 'G', 1));
+    addPiece(Piece(Piece::Type::KNIGHT, Piece::Color::BLACK, 'B', 1));
+    addPiece(Piece(Piece::Type::KNIGHT, Piece::Color::BLACK, 'G', 1));
 
-    boardState.push_back(Piece(Piece::Type::BISHOP, Piece::Color::BLACK, 'C', 1));
-    boardState.push_back(Piece(Piece::Type::BISHOP, Piece::Color::BLACK, 'F', 1));
+    addPiece(Piece(Piece::Type::BISHOP, Piece::Color::BLACK, 'C', 1));
+    addPiece(Piece(Piece::Type::BISHOP, Piece::Color::BLACK, 'F', 1));
 
-    boardState.push_back(Piece(Piece::Type::QUEEN, Piece::Color::BLACK, 'D', 1));
-    boardState.push_back(Piece(Piece::Type::KING, Piece::Color::BLACK, 'E', 1));
-
+    addPiece(Piece(Piece::Type::QUEEN, Piece::Color::BLACK, 'D', 1));
+    addPiece(Piece(Piece::Type::KING, Piece::Color::BLACK, 'E', 1));
 }
 
 int Board::getChessBoardPieceCount()
